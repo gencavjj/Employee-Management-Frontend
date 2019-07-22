44d807db43cdde1b3f1a0516c4777bca921c2afe
Jeremy <gencavjj@gmail.com>
2019-07-22 19:15:32 -0400

    updating employee list and details

--- Employee-Management-Frontend/src/app/employee-list/employee-list.component.ts
+++ Employee-Management-Frontend/src/app/employee-list/employee-list.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee';
 
 @Component({
-  selector: 'app-employee-list',
-  templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.css']
+  selector: 'employees-list',
+  templateUrl: './employees-list.component.html',
+  styleUrls: ['./employees-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
 
-  constructor() { }
+  employees: Observable<Employee[]>;
+
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
+    this.reloadData();
   }
 
+  deleteCustomers() {
+    this.employeeService.deleteAll()
+      .subscribe(
+        data => {
+          console.log(data);
+          this.reloadData();
+        },
+        error => console.log('ERROR: ' + error));
+  }
+
+  reloadData() {
+    this.employees = this.employeeService.getEmployeesList();
+  }
 }
