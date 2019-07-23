1e815b1e364c83a11fe72845855c96a15bfe000d
Jeremy <gencavjj@gmail.com>
2019-07-22 20:57:52 -0400

    added routing

--- Employee-Management-Frontend/src/app/app-routing.module.ts
+++ Employee-Management-Frontend/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
-
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+ 
+const routes: Routes = [
+    { path: '', redirectTo: 'employee', pathMatch: 'full' },
+    { path: 'employee', component: EmployeeListComponent },
+    { path: 'add', component: CreateEmployeeComponent },
+];
+ 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
+ 
 export class AppRoutingModule { }
--- Employee-Management-Frontend/src/app/app.module.ts
+++ Employee-Management-Frontend/src/app/app.module.ts
@@ -1,26 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+ 
 import { AppComponent } from './app.component';
-import { CreateEmployeeComponent } from './create-employee/create-employee/create-employee.component';
-import { EmployeeDetailsComponent } from './employee-details/employee-details/employee-details.component';
-import { EmployeeListComponent } from './employee-list/employee-list/employee-list.component';
-import { UpdateEmployeeComponent } from './update-employee/update-employee/update-employee.component';
-import { DeleteEmployeeComponent } from './delete-employee/delete-employee/delete-employee.component';
-
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+ 
 @NgModule({
   declarations: [
     AppComponent,
     CreateEmployeeComponent,
     EmployeeDetailsComponent,
     EmployeeListComponent,
-    UpdateEmployeeComponent,
-    DeleteEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
