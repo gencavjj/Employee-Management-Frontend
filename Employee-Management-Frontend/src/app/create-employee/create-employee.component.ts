0d65be0d579ed0e7314564c237be1dce0a8c87d1
Jeremy <gencavjj@gmail.com>
2019-07-22 20:51:35 -0400

    create-employee component

--- Employee-Management-Frontend/src/app/create-employee/create-employee.component.ts
+++ Employee-Management-Frontend/src/app/create-employee/create-employee.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+ 
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+ 
 @Component({
-  selector: 'app-create-employee',
+  selector: 'create-employee',
   templateUrl: './create-employee.component.html',
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
-
-  constructor() { }
-
+ 
+  employee: Employee = new Employee();
+  submitted = false;
+ 
+  constructor(private employeeService: EmployeeService) { }
+ 
   ngOnInit() {
   }
-
+ 
+  newCustomer(): void {
+    this.submitted = false;
+    this.employee = new Employee();
+  }
+ 
+  save() {
+    this.employeeService.createEmployee(this.employee)
+      .subscribe(data => console.log(data), error => console.log(error));
+    this.employee = new Employee();
+  }
+ 
+  onSubmit() {
+    this.submitted = true;
+    this.save();
+  }
 }
